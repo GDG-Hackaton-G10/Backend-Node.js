ed323186287833548712a9dc146aa07d49757a73
Barok-y <barokyeshibr@gmail.com>
2026-04-17 12:41:35 +0300

    Refractor/ Add Utility implementation as well as database connection

--- src/utils/responseFormatter.js
+++ src/utils/responseFormatter.js
@@ -1,11 +1,11 @@
-exports.sendSuccess = (res, data, statusCode = 200) => {
+export const sendSuccess = (res, data, statusCode = 200) => {
     res.status(statusCode).json({
         success: true,
         data
     });
 };
 
-exports.sendError = (res, message, code, statusCode = 400) => {
+export const sendError = (res, message, code, statusCode = 400) => {
     res.status(statusCode).json({
         success: false,
         error: { code, message }
