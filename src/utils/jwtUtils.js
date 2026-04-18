d90b3ee7a4dc5894479c2b01e73951b091c5849d
aneni <aneni.kidanu@aastustudent.edu.et>
2026-04-18 14:33:55 +0300

    Implement authentication system (register, login, JWT, middleware)

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -8,6 +8,15 @@ export const signToken = (id) => {
 
 export const signRefreshToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
+        expiresIn: process.env.JWT_REFRESH_EXPIRES_IN
     });
+
+};
+// VERIFY TOKEN
+export const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
+// VERIFY REFRESH TOKEN
+export const verifyRefreshToken = (token) => {
+  return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
 };
